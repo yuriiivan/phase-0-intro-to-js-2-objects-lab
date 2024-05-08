ddfc4c1cd5c9e04ff15efda301d6c15546932a2e
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-05-08 09:22:32 -0400

    Update employee nondestructively

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution in this file!
+const employee = {
+  name: 'Yuriy',
+  streetAddress: 'None of your business'
+};
+
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+  return {
+    ...employee,
+    [key]: value
+  };
+}
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+  return employee[key] = value;
+}
